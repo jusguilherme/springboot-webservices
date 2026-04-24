3b5bfaec8bead32090c60705a945409b4c88e58a
Justino Guilherme <163585641+jusguilherme@users.noreply.github.com>
2026-04-24 09:45:32 -0300

    feat: add JWT authentication, encrypt passwords with BCrypt and hide sensitive user data from API responses

--- src/main/java/com/project/webservices/auth/AuthController.java
+++ src/main/java/com/project/webservices/auth/AuthController.java
@@ -4,6 +4,7 @@ import com.project.webservices.config.JwtUtil;
 import com.project.webservices.entities.User;
 import com.project.webservices.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,12 +14,15 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/login")
     public String login(@RequestBody LoginRequest request) {
 
         User user = userRepository.findByEmail(request.getEmail());
 
-        if (user != null && user.getPassword().equals(request.getPassword())) {
+        if (user != null && passwordEncoder.matches(request.getPassword(), user.getPassword())) {
 
             String token = JwtUtil.generateToken(user.getEmail());
 
